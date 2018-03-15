b48c673a67c618cdac911a94781a9a5c15bd0f35
mgocmen <murat.dbc@gmail.com>
2018-03-14 18:38:13 -0700

    added files for starter kit

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 (function(){
   'use strict';
-  angular.module('CleaningQuote', ['ngMaterial'])
+  angular.module('app', ['ngMaterial','ui.router'])
+  .config(function($stateProvider,$urlRouterProvider){
+      
+  $urlRouterProvider.otherwise("/first")
+  var mainState = {
+    name: 'main',
+    url: '/main',
+    templateUrl:  '/partials/main.html'
+  }
+
+  var firstState = {
+    name: 'first',
+    url: '/first',
+    templateUrl:  '/partials/first.html'
+  }
+
+  $stateProvider.state(mainState);
+  $stateProvider.state(firstState);
+})
 
 }())
